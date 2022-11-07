bf444b48ffc826a5a59458538cce6c931d81aea9
Domingos Alexandre <domingos_alexandre@d.alexandre.jrInspiron-3501>
2022-11-07 20:23:01 -0300

    test mensagem

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,19 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import formImg from '../imgs/iluminado2.png';
 import "../styles/Formulario.scss";
+import toast, { Toaster } from 'react-hot-toast';
 
 
 export default function Formulario() {
-  const form = useRef();
+  const [form, setForm]  = useState({'user_email':'', 'user_name':'', message:''});
   console.log(process.env.REACT_APP_PUBLIC_KEY)
+
+  const handleChange = ({target}) => {
+    const { name, value } = target;
+    setForm({...form, [name]:value})
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+    emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID, form, process.env.REACT_APP_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
+          setForm({user_email:'', user_name:'', message:''})
+          toast.success('Menssagem enviada com sucesso');
       }, (error) => {
           console.log(error.text);
       });
@@ -23,17 +31,17 @@ export default function Formulario() {
     <section id='form_section'>
       <div className='form-img'>
         <img src={formImg} alt="imagem-do-formulario" />
-        <form ref={form} onSubmit={sendEmail}>
+        <form onSubmit={sendEmail}>
           <h2> Entre em Contato</h2>
-          <input type="text" name="user_name" placeholder='Nome'/>
+          <input onChange={handleChange} value={form['user_name']} type="text" name="user_name" placeholder='Nome'/>
          
-          <input type="email" name="user_email" placeholder='E-mail'/>
+          <input onChange={handleChange} value={form['user_email']} type="email" name="user_email" placeholder='E-mail'/>
           
-          <textarea name="message" placeholder='Sua Mensagem'/>
+          <textarea onChange={handleChange} value={form['message']} name="message" placeholder='Sua Mensagem'/>
           <button type="submit"> Enviar </button>
         </form>
       </div>
-     
+      <Toaster />
     </section>
   )
 };
